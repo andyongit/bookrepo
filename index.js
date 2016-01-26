1b2c17c8eb1529e759de3da259c4197094dba972
Andre Føyn Berge <git@foynberge.com>
2016-01-26 09:32:26 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var mongoRepository = require('./stockRepository')();
+var app = require('./app')(mongoRepository);
+
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
